47dcc15def4ae32da71eb816573abfbf2c67fb1d
Axator <paszynski.art@gmail.com>
2019-10-12 00:09:39 +0200

    Update missle calculations

--- Assets/Scripts/NewMain/Logic/Maths.cs
+++ Assets/Scripts/NewMain/Logic/Maths.cs
@@ -10,5 +10,22 @@ namespace BattlescapeLogic
         {
             return 1.0f / (1.0f + Mathf.Exp(-1.0f * x * growthRate));
         }
+
+
+        // these methods need testing
+        public static void SetObjectLocalYaw(GameObject gameObject, float angle)
+        {
+            gameObject.transform.localRotation *= Quaternion.AngleAxis(angle, new Vector3(0, 1, 0));
+        }
+
+        public static void SetObjectLocalPitch(GameObject gameObject, float angle)
+        {
+            gameObject.transform.localRotation *= Quaternion.AngleAxis(angle, new Vector3(0, 0, 1));
+        }
+
+        public static void SetObjectLocalRoll(GameObject gameObject, float angle)
+        {
+            gameObject.transform.localRotation *= Quaternion.AngleAxis(angle, new Vector3(1, 0, 0));
+        }
     }
 }
