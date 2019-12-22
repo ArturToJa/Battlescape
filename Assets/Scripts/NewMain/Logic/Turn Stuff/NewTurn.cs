aa9c5ad5aebb18fbf83f94cd8abcf37722fa0bf7
Lemur Dago <dagorlad111@o2.pl>
2019-12-22 01:20:59 +0100

    Added a folder for Turn relted stuff; Added Game Turn and New Turn into it;

---
+++ Assets/Scripts/NewMain/Logic/Turn Stuff/NewTurn.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace BattlescapeLogic
+{
+
+    public interface INewTurn
+    {
+        void OnNewTurn();
+        void OnCreation();
+        void OnDestruction();
+    }
+    public abstract class NewTurn : INewTurn
+    {
+        public NewTurn()
+        {
+            OnCreation();
+        }
+        public abstract void OnNewTurn();
+
+        public virtual void OnCreation()
+        {
+            GameTurn.instance.newTurnObjects.AddLast(this);
+        }
+
+        public virtual void OnDestruction()
+        {
+            GameTurn.instance.newTurnObjects.Remove(GameTurn.instance.newTurnObjects.Find(this));
+        }
+    }
+
+    public abstract class NewTurnMonoBehaviour : MonoBehaviour, INewTurn
+    {
+        public abstract void OnNewTurn();
+
+        public virtual void OnCreation()
+        {
+            GameTurn.instance.newTurnObjects.AddLast(this);
+        }
+
+        public virtual void OnDestruction()
+        {
+            GameTurn.instance.newTurnObjects.Remove(GameTurn.instance.newTurnObjects.Find(this));
+        }
+
+        protected virtual void Start()
+        {
+            OnCreation();
+        }
+    }
+}
