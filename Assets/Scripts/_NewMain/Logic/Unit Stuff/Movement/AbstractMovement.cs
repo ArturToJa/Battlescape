ec9213de755b49d6b2d1e92856e8f938573e412b
Lemur Dago <dagorlad111@o2.pl>
2020-03-25 01:06:37 +0100

    Bugfix - healthbars no longer jiggle when unit turns around

--- Assets/Scripts/_NewMain/Logic/Unit Stuff/Movement/AbstractMovement.cs
+++ Assets/Scripts/_NewMain/Logic/Unit Stuff/Movement/AbstractMovement.cs
@@ -36,7 +36,6 @@ namespace BattlescapeLogic
         protected void TurnTowards(Vector3 target)
         {
             Vector3 vector3 = new Vector3(target.x, myUnit.visuals.transform.position.y, target.z);
-            myUnit.transform.LookAt(vector3);
             //the following line does it in, i believe, one frame. If not, then forget what i said in comments in this file :D 
             myUnit.visuals.transform.LookAt(vector3);
         }
